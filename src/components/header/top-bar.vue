5f51224f9dbf2f7790ef21f16cc40f976b00495c
whispyy <jfdurk@gmx.fr>
2019-07-25 22:26:19 -0400

    chore(*): load icon lib

--- src/components/header/top-bar.vue
+++ src/components/header/top-bar.vue
@@ -2,23 +2,13 @@
   <div class="header" :class="{ dark: dark }">
     <div class="header-container">
       <div class="icon" @click="$emit('home')">
-        <svg viewBox="0 0 576 512">
-          <path
-            fill="currentColor"
-            d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"
-          ></path>
-        </svg>
+        <img src="@/assets/icon/home.svg" />
       </div>
 
       <h1 class="center">{{ title }}</h1>
 
       <div class="icon" @click="toggleMenu()">
-        <svg viewBox="0 0 448 512">
-          <path
-            fill="currentColor"
-            d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-          ></path>
-        </svg>
+        <img src="@/assets/icon/menu.svg" />
         <dropdown :open="openMenu" :position="{ top: 30, width: 200 }">
           <slot></slot>
         </dropdown>
