c4d8aec3aab8b2d2a049a34a60592021f145519e
whispyy <jfdurk@gmx.fr>
2019-07-25 22:26:19 -0400

    chore(*): load icon lib

--- src/components/header/score-bar.vue
+++ src/components/header/score-bar.vue
@@ -4,33 +4,18 @@
       <div class="item">
         <h3 class="title">{{ title }}</h3>
         <div class="icon" @click="$modal.show('edit-title')">
-          <svg viewBox="0 0 448 512">
-            <path
-              fill="currentColor"
-              d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-            ></path>
-          </svg>
+          <img src="@/assets/icon/pencil.svg" />
         </div>
       </div>
       <div class="item">
         <div class="icon" @click="$modal.show('item-add-edit')">
-          <svg viewBox="0 0 448 512">
-            <path
-              fill="currentColor"
-              d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-            ></path>
-          </svg>
+          <img src="@/assets/icon/add.svg" />
         </div>
       </div>
 
       <div class="item">
         <div class="icon" @click="close()">
-          <svg viewBox="0 0 448 512">
-            <path
-              fill="currentColor"
-              d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-            ></path>
-          </svg>
+          <img src="@/assets/icon/cancel.svg" />
         </div>
       </div>
     </div>
